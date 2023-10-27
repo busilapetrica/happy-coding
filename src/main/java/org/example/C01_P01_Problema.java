be348171455977230e24cb4d86bdabf4043264e9
Petrica <41289735+pet5555@users.noreply.github.com>
2023-10-27 18:31:25 +0300

    add  write problem first ten natural numbers C01_p01

--- src/main/java/org/example/C01_P01_Problema.java
+++ src/main/java/org/example/C01_P01_Problema.java
@@ -3,6 +3,12 @@ package org.example;
 public class C01_P01_Problema {
 
     public static void main(String[] args) {
-
+       /* Write a program in java to display the first 10 natural numbers.
+        Expected Output :
+        1 2 3 4 5 6 7 8 9 10*/
+        int number = 10;
+        for(int i=0; i<=number; i++){
+            System.out.println(i);
+        }
     }
 }
