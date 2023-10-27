646eae7a5e2c4fd4cd85b614a17c64c1225aa9e7
Petrica <41289735+pet5555@users.noreply.github.com>
2023-10-27 18:48:02 +0300

    add problem C03_02 get version of java

---
+++ src/main/java/org/example/C03_02_GetversionOfJava.java
@@ -0,0 +1,6 @@
+package org.example;
+public class C03_02_GetversionOfJava {
+        public static void main(String[] args){
+            System.out.println("Java runtime version: " + System.getProperty("java.runtime.version" ));
+    }
+}
