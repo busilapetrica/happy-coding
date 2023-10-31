1c0db01b69ae8c253788e37e67ad8ab141f45e21
Petrica <41289735+pet5555@users.noreply.github.com>
2023-10-31 20:08:18 +0200

    add problem Cercle_Perimeter and Area

---
+++ src/main/java/org/example/C03_P06_Cercle_Perimeter_Area.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.lang.Math.*;
+public class C03_P06_Cercle_Perimeter_Area {
+    public static void main(String[] args){
+
+        /* Write a C program to compute the perimeter and area of a circle with a given radius.
+Expected Output:
+Perimeter of the Circle = 37.680000 inches
+Area of the Circle = 113.040001 square inches*/
+
+         double pi = 3.14;
+         double radius = 12.0;
+         double cercle_perimeter = 2 * pi * radius;
+         double cercle_area = pi * Math.pow(radius, 2);
+         
+         System.out.println(cercle_perimeter);
+         System.out.println(cercle_area);
+    }
+}
