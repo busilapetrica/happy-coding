800ad55c541294f2b171b8b003ab788f8ee074a3
Petrica <41289735+pet5555@users.noreply.github.com>
2023-10-31 19:24:21 +0200

    update problem version of java

--- src/main/java/org/example/C01_02_GetversionOfJava.java
+++ src/main/java/org/example/C01_02_GetversionOfJava.java
@@ -1,5 +1,5 @@
 package org.example;
-public class C03_02_GetversionOfJava {
+public class C01_02_GetversionOfJava {
         public static void main(String[] args){
             System.out.println("Java runtime version: " + System.getProperty("java.runtime.version" ));
     }
