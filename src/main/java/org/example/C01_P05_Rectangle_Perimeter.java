da40709d64780a6dcae436e2c4d95076816857a3
Petrica <41289735+pet5555@users.noreply.github.com>
2023-10-31 19:50:45 +0200

    add problem perimeter of rectangle

---
+++ src/main/java/org/example/C01_P05_Rectangle_Perimeter.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class C01_P05_Rectangle_Perimeter {
+/*Write a C program to compute the perimeter and area of a rectangle with a height of 7 inches and width of 5 inches.
+Expected Output:
+Perimeter of the rectangle = 24 inches
+Area of the rectangle = 35 square inches*/
+        public static void main(String[] args){
+            int x = 24;
+            int y = 35;
+            int result = 2 * (x + y);
+            System.out.print(result);
+        }
+}
