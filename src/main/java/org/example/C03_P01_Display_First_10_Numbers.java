85bd677f6ca6d3df7db3401090f3f939b2be9075
Petrica <41289735+pet5555@users.noreply.github.com>
2023-11-22 20:09:57 +0200

    add problem display first ten numbers

---
+++ src/main/java/org/example/C03_P01_Display_First_10_Numbers.java
@@ -0,0 +1,13 @@
+package org.example;
+/*****************************************************************************
+ * C03_P01_Display_10_Numbers
+ Write a program in java to display the first 10 numbers.
+ Output:1 2 3 4 5 6 7 8 9 10
+ *******************************************************************************/
+public class C03_P01_Display_First_10_Numbers {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
