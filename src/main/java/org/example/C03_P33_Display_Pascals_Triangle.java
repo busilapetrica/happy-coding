18436e51b68385e87a05b669b5af2bcd68360598
Petrica <41289735+pet5555@users.noreply.github.com>
2023-12-04 22:21:05 +0200

    add problem display pascals trangle

---
+++ src/main/java/org/example/C03_P33_Display_Pascals_Triangle.java
@@ -0,0 +1,37 @@
+package org.example;
+/*
+Write a java program to display Pascal's triangle.
+ Test Data :
+Input number of rows: 5
+Expected Output :
+        1
+      1   1
+    1   2   1
+  1   3   3   1
+1   4   6   4   1
+ */
+public class C03_P33_Display_Pascals_Triangle {
+    public static void main(String[] args) {
+        int rows = 4;
+        int count = 1;
+
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+
+            int currentNumber = count;
+            while (currentNumber != 0) {
+                System.out.print(currentNumber % 10 + "   ");
+                currentNumber /= 10;
+            }
+
+            count *= 11;
+            System.out.println();
+        }
+    }
+}
+
+
+
+
