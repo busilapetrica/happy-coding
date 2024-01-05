8eff8efe8b520d8bcdabf3700cb5d73594d7c54d
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-05 21:50:32 +0200

    modify problem display array elements . adding scanner and changed the algorithm because was not working properly

--- src/main/java/org/example/C04_P01_Display_Array_Elements.java
+++ src/main/java/org/example/C04_P01_Display_Array_Elements.java
@@ -1,30 +1,38 @@
 package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
 /******************************************************************************
  Write a program in java to store elements in an array and print them.
  Test Data :
  Input 10 elements in the array :
- element - 0 : 1
- element - 1 : 1
- element - 2 : 2
+ element - 0 : 33
+ element - 1 : 42
+ element - 2 : 51
  .......
  Expected Output :
- Elements in array are: 1 1 2 3 4 5 6 7 8 9
+ Elements in array are: 33 42 51
  *******************************************************************************/
-public class C04_P01_Display_Array_Elements{
+public class C04_P01_Display_Array_Elements {
     public static void main(String[] args) {
-        int[] array = new int[10];
-        int[] newArray =  addElementsToArray(array);
-        //display elements from array
-        for (int i : newArray) {
-            System.out.print(i + " ");
-        }
-    }
-    public static int[] addElementsToArray(int[] array) {
+        System.out.println("Number of elements:");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[n];
 
-        array[0] = 1;
-    for (int i=1; i<=9; i++){
-        array[i] = i;
+        for (int i = 0; i < n; i++) {
+            System.out.println("element - " + i + " : ");
+            array[i] = in.nextInt();
+        }
+
+        // Convert array to string
+        String result = Arrays.toString(addElementsToArray(array));
+        System.out.println(result);
     }
-    return array;
+
+    public static int[] addElementsToArray(int[] array) {
+        int[] result = new int[array.length];
+        System.arraycopy(array, 0, result, 0, array.length);
+        return result;
     }
 }
