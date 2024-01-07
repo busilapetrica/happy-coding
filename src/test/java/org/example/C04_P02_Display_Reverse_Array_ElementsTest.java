ee04a4a39a24f896aec36686520b9d865c568acc
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-07 13:21:04 +0200

    changed display reverse array elements problem test to compare the result correctly . the test was copied from previous test problem

--- src/test/java/org/example/C04_P02_Display_Reverse_Array_ElementsTest.java
+++ src/test/java/org/example/C04_P02_Display_Reverse_Array_ElementsTest.java
@@ -1,10 +1,4 @@
-package org.example;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class C04_P02_Display_Reverse_Array_ElementsTest {
     package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -13,32 +7,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
-    class C04_P01_Display_Array_ElementsTest {
+    class C04_P02_Display_Reverse_Array_ElementsTest {
         @Test
-        public void convertNumbersToCommaSeparatedList() {
-            int[] array = {1, 1, 5, 3, 4, 5, 6, 7, 8, 9}; // Initial array
+        public void convertNumbersToCommaSeparatedListReversed() {
+            int[] array = {1, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // Initial array
             // Call logic
-            String result = C04_P01_Display_Array_Elements.arrayToString(array);
+            String result = C04_P02_Display_Reverse_Array_Elements.arrayToStringRevers(array);
             // Assert result
-            assertEquals("1, 1, 5, 3, 4, 5, 6, 7, 8, 9", result);
+            assertEquals("9, 8, 7, 6, 5, 4, 3, 2, 1, 1", result);
         }
 
         @Test
-        public void convert3NumbersToBlankSpaceSeparatedList() {
+        public void convert3NumbersToBlankSpaceSeparatedListReversed() {
             int[] array = {33, 42, 51}; // Initial array
             String result = C04_P02_Display_Reverse_Array_Elements.arrayToStringRevers(array);
             // Assert result
-            assertEquals("33, 42, 51", result);
+            assertEquals("51, 42, 33", result);
         }
 
         @Test
-        public void convert2NumbersToBlankSpaceSeparatedList() {
+        public void convert2NumbersToBlankSpaceSeparatedListReversed() {
             int[] array = {3, 4}; // Initial array
-            String result = C04_P01_Display_Array_Elements.arrayToString(array);
+            String result = C04_P02_Display_Reverse_Array_Elements.arrayToStringRevers(array);
             // Assert result
-            assertEquals("3, 4", result);
+            assertEquals("4, 3", result);
         }
 
 
     }
-}
+
