e7ba53cc6d97e97b38823e21b1b9fe6812a13414
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-07 18:46:54 +0200

    add array copy test

---
+++ src/test/java/org/example/C04_P04_Array_CopyTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class C04_P04_Array_CopyTest {
+    @Test
+    public void copyNumbersToCommaSeparatedList() {
+        int[] array = {1, 1, 5, 3, 4, 5, 6, 7, 8, 9}; // Initial array
+        // Call logic
+        String result = C04_P04_Array_Copy.copyToSecondArray(array);
+        // Assert result
+        assertEquals("1, 1, 5, 3, 4, 5, 6, 7, 8, 9", result);
+    }
+
+    @Test
+    public void copy3NumbersToCommaSpaceSeparatedList() {
+        int[] array = {33, 42, 51}; // Initial array
+        String result = C04_P04_Array_Copy.copyToSecondArray(array);
+        // Assert result
+        assertEquals("33, 42, 51", result);
+    }
+
+    @Test
+    public void copy2NumbersToCommaSpaceSeparatedList() {
+        int[] array = {3, 4}; // Initial array
+        String result = C04_P04_Array_Copy.copyToSecondArray(array);
+        // Assert result
+        assertEquals("3, 4", result);
+    }
+
+
+}
