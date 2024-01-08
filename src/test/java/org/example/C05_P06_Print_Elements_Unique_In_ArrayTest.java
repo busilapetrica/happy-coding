606f3d83f8b0fafc758446ca2e04dd7de29db534
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-08 21:06:43 +0200

    add print elements unique in array test

---
+++ src/test/java/org/example/C05_P06_Print_Elements_Unique_In_ArrayTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class C05_P06_Print_Elements_Unique_In_ArrayTest {
+
+        @Test
+        public void findOneUniqueElement() {
+            int[] array = {1, 1, 3}; // Initial array
+            // Call logic
+            String result = C05_P06_Print_Elements_Unique_In_Array.uniqueElementsFromArray(array);
+            // Assert result
+            assertEquals("3", result);
+        }
+@Test
+    public void findTwoUniqueElementsToCommaSeparatedList() {
+        int[] array = {1, 1, 5, 3, 5, 6}; // Initial array
+        // Call logic
+        String result = C05_P06_Print_Elements_Unique_In_Array.uniqueElementsFromArray(array);
+        // Assert result
+        assertEquals("3, 6", result);
+    }
+
+    @Test
+    public void findThreeUniqueElementsToCommaSeparatedList() {
+        int[] array = {1, 1, 5, 3, 3, 6, 7, 8, 8}; // Initial array
+        // Call logic
+        String result = C05_P06_Print_Elements_Unique_In_Array.uniqueElementsFromArray(array);
+        // Assert result
+        assertEquals("5, 6, 7", result);
+    }
+    }
