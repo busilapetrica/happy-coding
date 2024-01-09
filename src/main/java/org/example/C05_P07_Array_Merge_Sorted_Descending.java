c3a4285d82fc97106fd28436e84459d3aae2ae9d
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-09 13:33:15 +0200

    Update C05_P07_Array_Merge_Sorted_Descending.java
    add problem array merge sorted descending

--- src/main/java/org/example/C05_P07_Array_Merge_Sorted_Descending.java
+++ src/main/java/org/example/C05_P07_Array_Merge_Sorted_Descending.java
@@ -1,4 +1,73 @@
 package org.example;
-/*#######################
-public class C05_P07_Array_Merge_Sorted_Descending {
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Collections;
+/*****************************************************
+Write a program in C to merge two arrays of the same size sorted in descending order.
+Test Data :
+Input the number of elements to be stored in the first array :3
+Input 3 elements in the array :
+element - 0 : 1
+element - 1 : 2
+element - 2 : 3
+Input the number of elements to be stored in the second array :3
+Input 3 elements in the array :
+element - 0 : 1
+element - 1 : 2
+element - 2 : 3
+Expected Output :
+The merged array in decending order is :
+3, 3, 2, 2, 1, 1
+*****************************************************/
+class C05_P07_Array_Merge_Sorted_Descending {
+    public static void main(String[] args) {
+System.out.println("Number of elements : ");
+Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+ 
+        int[] array1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter element - " + i + " - of array 1:");
+            array1[i] = scanner.nextInt();
+        }
+ 
+        int[] array2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter element - " + i + " - of array 2:");
+            array2[i] = scanner.nextInt();
+        }
+        String result = mergeArraysAndSorting(array1, array2);
+        System.out.println(result);
+    }
+ 
+    public static String mergeArraysAndSorting(int[] array1, int[] array2) {
+        int arraysLength = array1.length + array2.length;
+        int[] arrayMerge = new int[arraysLength];
+ 
+        // Copy elements from array1
+        for (int i = 0; i < array1.length; i++) {
+            arrayMerge[i] = array1[i];
+        }
+ 
+        // Copy elements from array2
+        for (int i = 0; i < array2.length; i++) {
+            arrayMerge[array1.length + i] = array2[i];
+        }
+     String result = makeStringFromArray(arrayMerge);
+     
+     return result;
+    }
+    
+    public static String makeStringFromArray(int[] arrayMerge){
+        Arrays.sort(arrayMerge);
+         String reverseString = "";
+// Build the result reverse string 
+        for (int i = arrayMerge.length - 1; i >= 0; i--) {
+    reverseString += arrayMerge[i];
+    if (i > 0) {
+        reverseString += ", ";
+    }
+}
+return reverseString;
+    }
 }
