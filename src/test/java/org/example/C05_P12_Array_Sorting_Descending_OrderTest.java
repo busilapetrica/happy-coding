0fc0c7ecb64133535fb78cebe6a3f2e7b19030fd
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-17 20:40:07 +0200

    add problem and test for array sorting descending order

---
+++ src/test/java/org/example/C05_P12_Array_Sorting_Descending_OrderTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class C05_P12_Array_Sorting_Descending_OrderTest {
+    @Test
+    public void findAndSortTenElementsInDescendingOrder() {
+        int[] array = {1, 3, 2, 7, 9, 4, 5, 8, 6};
+        int[] result = C05_P12_Array_Sorting_Descending_Order.sortingArrayInDescendingOrder(array);
+        assertEquals("[9, 8, 7, 6, 5, 4, 3, 2, 1]", Arrays.toString(result));
+    }
+    @Test
+    public void findAndSortFiveElementsInDescendingOrder() {
+        int[] array = {1, 3, 2, 7, 9};
+        int[] result = C05_P12_Array_Sorting_Descending_Order.sortingArrayInDescendingOrder(array);
+        assertEquals("[9, 7, 3, 2, 1]", Arrays.toString(result));
+    }
+
+}
