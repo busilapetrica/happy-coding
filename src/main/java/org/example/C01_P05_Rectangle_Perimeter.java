3a6072e44772a1501e626d02e00db9cf33b01d72
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-20 10:32:54 +0200

    add test for problem rectangle perimeter ..and modify the problem to retun the perimeter of rectangle

--- src/main/java/org/example/C01_P05_Rectangle_Perimeter.java
+++ src/main/java/org/example/C01_P05_Rectangle_Perimeter.java
@@ -1,14 +1,13 @@
 package org.example;
-
+/****************************************
+ /*Write a java program to compute the perimeter and area of a rectangle with a height of 7 inches and width of 5 inches.
+ Expected Output:
+ Perimeter of the rectangle = 24 inches
+ Area of the rectangle = 35 square inches
+ ***************************************/
 public class C01_P05_Rectangle_Perimeter {
-/*Write a C program to compute the perimeter and area of a rectangle with a height of 7 inches and width of 5 inches.
-Expected Output:
-Perimeter of the rectangle = 24 inches
-Area of the rectangle = 35 square inches*/
-        public static void main(String[] args){
-            int x = 24;
-            int y = 35;
-            int result = 2 * (x + y);
-            System.out.print(result);
-        }
+    public static int calculatePerimeterOfRectangle(int height, int width) {
+        int perimeter = 2 * (height + width);
+        return perimeter;
+    }
 }
