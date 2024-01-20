c1665311293eda4642e8545dda1b5ef117568125
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-20 11:59:01 +0200

    add problem and test for problem add new element to unsorted array

--- src/main/java/org/example/C05_P14_Add_New_Element_To_Unsorted_Array.java
+++ src/main/java/org/example/C05_P14_Add_New_Element_To_Unsorted_Array.java
@@ -1,31 +1,30 @@
 package org.example;
 
-/****************************************************8
- Write a program in java to insert the values in the array (sorted list).
+/***************************************
+ Write a program in java to insert values in the array (unsorted list).
  Test Data :
- Insert New value in the sorted array :
- -----------------------------------------
- Input the size of array : 5
- Input 5 elements in the array in ascending order:
- element - 0 : 2
- element - 1 : 5
+ Input the size of array : 4
+ Input 4 elements in the array in ascending order:
+ element - 0 : 1
+ element - 1 : 8
  element - 2 : 7
- element - 3 : 9
- element - 4 : 11
- Input the value to be inserted : 8
- The exist array list is :
- 2 5 7 9 11
- After Insert the list is :
- 2 5 7 8 9 11
- */
-public class C012_P13_Add_New_Element_To_Array {
-    public static int[] addElementToArray(int[] array, int newElement){
+ element - 3 : 10
+ Input the value to be inserted : 5
+ Input the Position, where the value to be inserted :2
+ Expected Output :
+ The current list of the array :
+ 1 8 7 10
+ After Insert the element the new list is :
+ 1 5 8 7 10
+ ********************************************/
+public class C05_P14_Add_New_Element_To_Unsorted_Array {
+    public static int[] addElementToArray(int[] array, int newElement) {
         int[] newArray = new int[array.length + 1];
 
         int newIndex = 0;
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] < newElement) {
+            if (i < 1) {
                 newArray[newIndex++] = array[i];
             } else {
                 // Always add the new element after any equal or greater elements
@@ -37,11 +36,10 @@ public class C012_P13_Add_New_Element_To_Array {
                 break;
             }
         }
-        
         if (newIndex < newArray.length) {
             newArray[newIndex] = newElement;
         }
-
         return newArray;
     }
 }
+
--- src/test/java/org/example/C05_P13_Add_New_Element_To_ArrayTest.java
+++ src/test/java/org/example/C05_P13_Add_New_Element_To_ArrayTest.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class C012_P13_Add_New_Element_To_ArrayTest {
+class C05_P13_Add_New_Element_To_ArrayTest {
 
     @Test
     public void addNewElementToArrayWithAscendingOrder() {
         int[] array = {2, 5, 7, 9, 11};
-        int[] result = C012_P13_Add_New_Element_To_Array.addElementToArray(array, 8);
+        int[] result = C05_P13_Add_New_Element_To_Array.addElementToArray(array, 8);
         assertEquals("[2, 5, 7, 8, 9, 11]", Arrays.toString(result));
     }
 
