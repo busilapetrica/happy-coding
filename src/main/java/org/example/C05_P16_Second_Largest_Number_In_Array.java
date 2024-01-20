d45ec9acc8c9197a2a3599283589723868600ffd
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-20 12:40:56 +0200

    add problem and test second largest number in array

---
+++ src/main/java/org/example/C05_P16_Second_Largest_Number_In_Array.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class C05_P16_Second_Largest_Number_In_Array {
+    public static void main(String[] args) {
+        int[] myNum = {5, 2, 5, 5, 7};
+        int result = findSecondLargestElement(myNum);
+        System.out.print(result);
+    }
+    public static int findSecondLargestElement(int[] array) {
+
+        if (array.length == 2) {
+            if (array[0] > array[1]) {
+                return array[1];
+            } else {
+                return array[0];
+            }
+        } else {
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = 0; j < array.length - i - 1; j++) {
+                    if (array[j] < array[j + 1]) {
+
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array[1];
+        }
+    }
+}
+
---
+++ src/test/java/org/example/C05_P16_Second_Largest_Number_In_ArrayTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class C05_P16_Second_Largest_Number_In_ArrayTest {
+    @Test
+    public void returnSecondLargestNumberFromArrayWith5Numbers() {
+        int[] array = {1, 3, 2, 7, 9};
+        int result = C05_P16_Second_Largest_Number_In_Array.findSecondLargestElement(array);
+        assertEquals(7, result);
+    }
+@Test
+    public void returnSecondLargestNumberFromArrayWith2ElementsWhereSecondIsBiggest() {
+        int[] array = {7, 9};
+        int result = C05_P16_Second_Largest_Number_In_Array.findSecondLargestElement(array);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void returnSecondLargestNumberFromArrayWith2ElementsWhereFirstIsBiggest() {
+        int[] array = {9, 7};
+        int result = C05_P16_Second_Largest_Number_In_Array.findSecondLargestElement(array);
+        assertEquals(7, result);
+    }
+
+}
