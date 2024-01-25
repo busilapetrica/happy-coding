e97dc079f86cc1012d1964260e9c7e207417f6d2
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-25 22:13:27 +0200

    add test for problem count positive negative numbers

--- src/main/java/org/example/C01_P27_Count_Positive_Negative_Numbers.java
+++ src/main/java/org/example/C01_P27_Count_Positive_Negative_Numbers.java
@@ -1,7 +1,6 @@
 package org.example;
-
 import java.util.Scanner;
-/*
+/**********************************************
 Write a C program that reads 5 numbers and counts the number of positive numbers and negative numbers.
 Test Data :
 Input the first number: 5
@@ -11,23 +10,22 @@ Input the fourth number: 15
 Input the fifth number: -1
 Expected Output:
 Number of positive numbers: 3
-Number of negative numbers: 2 */
+Number of negative numbers: 2
+ ********************************************/
 public class C01_P27_Count_Positive_Negative_Numbers {
-    public static void main(String[] args) {
-
+    static String countNumbersPositiveAndNegative(int[] array) {
+        String result ="";
         int count_positive = 0, count_negative = 0;
         Scanner in = new Scanner(System.in);
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("enter first integer x" + i + ": ");
-            int x = in.nextInt();
-            if (x  < 0) {
+        for (int i = 0; i <= array.length -1; i++) {
+
+            if (array[i]  < 0) {
                 count_negative = count_negative + 1;
             }else{
                 count_positive = count_positive + 1;
             }
         }
-        System.out.println("Positive numbers: " + count_positive);
-        System.out.println("Negative numbers: " + count_negative);
-
+        result = "Positive: " + count_positive + "\nNegative: " + count_negative + "\n";
+        return result;
     }
 }
---
+++ src/test/java/org/example/C01_P27_Count_Positive_Negative_NumbersTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class C01_P27_Count_Positive_Negative_NumbersTest {
+    @Test
+    public void returnSumOfArrayWith5OddNumbers() {
+        int[] array = new int[]{5,-4,10,15,-1};
+        String result = C01_P27_Count_Positive_Negative_Numbers.countNumbersPositiveAndNegative(array);
+        assertEquals("""
+                Positive: 3
+                Negative: 2
+                """ , result);
+    }
+}
