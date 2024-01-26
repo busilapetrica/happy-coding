eb712c2e8dd49087c89c3eac72fcc51ce05a84eb
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-26 22:41:07 +0200

    add test for problem triangle increasing line

---
+++ src/test/java/org/example/C03_P12_Triangle_Increasing_LineTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class C03_P12_Triangle_Increasing_LineTest {
+    @Test
+    public void returnPatternTriangleWithNumberIncreasingWith4Rows() {
+        String result = C03_P12_Triangle_Increasing_Line.patternTriangle(4);
+        assertEquals("""
+                                1
+                                23
+                                456
+                                78910
+                                """, result);
+    }
+}
