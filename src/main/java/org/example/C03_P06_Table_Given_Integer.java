df81d1b0401e2f4ddd961c8704b4159a8d13c242
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-26 21:06:09 +0200

    add test for problem table given integer

--- src/main/java/org/example/C03_P06_Table_Given_Integer.java
+++ src/main/java/org/example/C03_P06_Table_Given_Integer.java
@@ -1,20 +1,17 @@
 package org.example;
 
 import java.util.Scanner;
-
 /*****************************************************************************
  Write program in java to to display multiplication table for a given integer..
  EX: Input the number : 15
  Output : 15X1=15
  *******************************************************************************/
 public class C03_P06_Table_Given_Integer {
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-
-        for(int i=1; i<=10; i++){
-            System.out.println(x + "X" + i +"=" + i*x);
+    static String displayMultiplicationTable(int number) {
+        String result = "";
+        for (int i = 1; i <= 10; i++) {
+            result += number + "X" + i + "=" + i * number + "\n";
         }
+        return result;
     }
 }
