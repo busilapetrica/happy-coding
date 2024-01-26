129b6f88ab12df1f25cb6ec8e8f469b386e02aa0
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-26 21:50:44 +0200

    add test for triangle repeat number row

--- src/main/java/org/example/C03_P11_Triangle_Repeat_Number_Row.java
+++ src/main/java/org/example/C03_P11_Triangle_Repeat_Number_Row.java
@@ -8,13 +8,14 @@ package org.example;
  4444
  *******************************************************************************/
 public class C03_P11_Triangle_Repeat_Number_Row {
-    public static void main(String[] args) {
-        int x = 4;
-        for (int i = 1; i <= x; i++) {
+    public static String triangleRepeatNumber(int number) {
+        String result ="";
+        for (int i = 1; i <= number; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i);
+                result += i;
             }
-            System.out.println();
+            result +="\n";
         }
+        return result;
     }
 }
---
+++ src/test/java/org/example/C03_P11_Triangle_Repeat_Number_RowTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class C03_P11_Triangle_Repeat_Number_RowTest {
+    @Test
+    public void returnPatternTriangleWithSameNumberOnRow() {
+        String result = C03_P11_Triangle_Repeat_Number_Row.triangleRepeatNumber(4);
+        assertEquals("""
+                                1
+                                22
+                                333
+                                4444
+                                """, result);
+    }
+
+}
