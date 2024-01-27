5fb8594b13571540bf87f4a0934f214ce58deb69
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-27 11:12:59 +0200

    add test to problem display pascals triangle

--- src/main/java/org/example/C03_P33_Display_Pascals_Triangle.java
+++ src/main/java/org/example/C03_P33_Display_Pascals_Triangle.java
@@ -11,24 +11,25 @@ Expected Output :
 1   4   6   4   1
  */
 public class C03_P33_Display_Pascals_Triangle {
-    public static void main(String[] args) {
-        int rows = 5;
+    public static String pascalsTriangle(int rows) {
+        String result = "";
         int count = 1;
 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= rows - i; j++) {
-                System.out.print("  ");
+                result += " ";
             }
 
             int currentNumber = count;
             while (currentNumber != 0) {
-                System.out.print(currentNumber % 10 + "   ");
+                result += currentNumber % 10 + "  ";
                 currentNumber /= 10;
             }
 
             count *= 11;
-            System.out.println();
+            result += "\n";
         }
+        return result;
     }
 }
 
