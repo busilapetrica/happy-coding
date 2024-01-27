ec52c1c4fde6b5c27a9a5cf8b51c3ff49bdfdf83
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-27 11:21:34 +0200

    add test for problem Fibonacci numbers and delete main

--- src/main/java/org/example/C03_P35_Fibonacci_Series.java
+++ src/main/java/org/example/C03_P35_Fibonacci_Series.java
@@ -1,26 +1,29 @@
 package org.example;
-/*Write a program in C to display the first n terms of the Fibonacci series.
-Fibonacci series 0 1 2 3 5 8 13 .....
-Test Data :
-Input number of terms to display : 10
-Expected Output :
-Here is the Fibonacci series upto to 10 terms :
-0 1 1 2 3 5 8 13 21 34
- F(n)=F(n−1)+F(n−2)*/
+/*************************************************
+ Write a program in java to display the first n terms of the Fibonacci series.
+ Fibonacci series 0 1 2 3 5 8 13 .....
+ Test Data :
+ Input number of terms to display : 10
+ Expected Output :
+ Here is the Fibonacci series upto to 10 terms :
+ 0 1 1 2 3 5 8 13 21 34
+ F(n)=F(n−1)+F(n−2)
+ **************************************************/
 public class C03_P35_Fibonacci_Series {
-    public static void main(String[] args){
+    public static String fibonacciNumbers(int countFibonacciNumbers) {
         int previousNumber = 0;
         int lastNumber = 1;
-        int count = 10;
+        String result = "";
 
-        System.out.print(previousNumber + " " + lastNumber);
+        result += previousNumber + " " + lastNumber;
 
-        for (int i = 3; i <= count; i++) {
+        for (int i = 3; i <= countFibonacciNumbers; i++) {
             int currentFibonacciNumber = previousNumber + lastNumber;
-            System.out.print(" " + currentFibonacciNumber);
+            result += " " + currentFibonacciNumber;
 
             previousNumber = lastNumber;
             lastNumber = currentFibonacciNumber;
         }
+        return result;
     }
 }
