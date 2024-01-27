3f75c28ef75a68e2d75e93b51ce9ff2783b85ca5
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-27 11:30:33 +0200

    add test for problem Pattern increase decrease rows

--- src/main/java/org/example/C03_P36_Pattern_Increase_Decrease_Row.java
+++ src/main/java/org/example/C03_P36_Pattern_Increase_Decrease_Row.java
@@ -2,25 +2,25 @@ package org.example;
 /******************************************************************************
  Write a java program to display a such a pattern for n rows using a number that starts with 1 and each
  row will have a 1 as the first and last number.
- 1
- 121
+   1
+  121
  12321
  *******************************************************************************/
-
 public class C03_P36_Pattern_Increase_Decrease_Row {
-    public static void main (String[]args){
-        int rows = 3;
-        for (int i = 1; i <= rows; i++){
-            for(int j=1; j<=rows-i; j++){
-                System.out.print(" ");
+    public static String patternIncreaseAndDecreaseNumbers(int rows) {
+        String result = "";
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                result += " ";
             }
-            for (int k = 1; k <= i; k++){
-                System.out.print (k);
+            for (int k = 1; k <= i; k++) {
+                result += k;
             }
-            for (int l = i-1; l >= 1; l--){
-                System.out.print (l);
+            for (int l = i - 1; l >= 1; l--) {
+                result += l;
             }
-            System.out.println ();
+            result += "\n";
         }
+        return result;
     }
 }
---
+++ src/test/java/org/example/C03_P36_Pattern_Increase_Decrease_RowTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class C03_P36_Pattern_Increase_Decrease_RowTest {
+    @Test
+    public void returnIncreaseAndDecreasePatternWith5Rows(){
+        int rows = 5;
+        String result = C03_P36_Pattern_Increase_Decrease_Row.patternIncreaseAndDecreaseNumbers(rows);
+        assertEquals("""
+                                1
+                               121
+                              12321
+                             1234321
+                            123454321
+                                 """, result);
+    }
+}
