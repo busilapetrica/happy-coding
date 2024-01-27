ea7b05e4f686948ec4d78a1d3ce779fcf83db0c9
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-27 12:01:37 +0200

    delete main method from problem palindrome number

--- src/main/java/org/example/C03_P38_Palindrome_Number.java
+++ src/main/java/org/example/C03_P38_Palindrome_Number.java
@@ -1,22 +1,10 @@
 package org.example;
-
 /******************************************************************************
  Write a java program to check whether a number is a palindrome or not.
  number = 121
  Expect output:  121 is a palindrome number
  *******************************************************************************/
 public class C03_P38_Palindrome_Number {
-    public static void main (String[]args){
-
-        //input
-        int number = 121;
-
-        //logica(algoritmica)
-        String message = isPalindrome(number);
-        //output
-        System.out.print(message);
-    }
-
     public static String isPalindrome(int number) {
         int originalNumber = number;
         int reversedNumber = 0;
@@ -26,7 +14,7 @@ public class C03_P38_Palindrome_Number {
             reversedNumber = reversedNumber * 10 + lastDigit;
             number /= 10;
         }
-
+        
         String message;
         if (originalNumber == reversedNumber){
             message = "The number " + originalNumber + " is palindrome";
