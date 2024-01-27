1152759daf7be19ec3e599418d9b82e88f369d23
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-27 12:11:49 +0200

    add test for problem number divisible with 9

--- src/main/java/org/example/C03_P39_Number_Divisible_With_Nine.java
+++ src/main/java/org/example/C03_P39_Number_Divisible_With_Nine.java
@@ -1,14 +1,17 @@
 package org.example;
+
 /******************************************************************************
  Write a java program to find the number and sum off all integer between 100 and 200 which are divisible with 9.
  Output : 108 117 126 135 144 153 162 171 180 189 198
  *******************************************************************************/
-public class C03_P39_Number_Divisible_With_Nine{
-    public static void main (String[]args){
-        for(int i=100; i <= 200; i++){
-            if(i%9==0){
-                System.out.print(i + " ");
+public class C03_P39_Number_Divisible_With_Nine {
+    public static String sumNumbersDivided(int fromNumber, int toNumber, int divNumber) {
+        String result="";
+        for (int i = fromNumber; i <= toNumber; i++) {
+            if (i % divNumber == 0) {
+               result += i + " ";
             }
         }
+        return result;
     }
 }
