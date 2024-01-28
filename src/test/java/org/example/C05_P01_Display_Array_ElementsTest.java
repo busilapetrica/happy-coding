c246e7f65e6d8823e02e3b2ac11714bc607ad34a
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-28 12:11:02 +0200

    deleted some repeated test from problem display array elements

--- src/test/java/org/example/C05_P01_Display_Array_ElementsTest.java
+++ src/test/java/org/example/C05_P01_Display_Array_ElementsTest.java
@@ -14,22 +14,4 @@ class C05_P01_Display_Array_ElementsTest {
         // Assert result
         assertArrayEquals(new int[]{1, 1, 5, 3, 4, 5, 6, 7, 8, 9}, result);
     }
-
-    @Test
-    public void return3ElementsInArrayList() {
-        int[] array = {33, 42, 51}; // Initial array
-        int[] result = C05_P01_Display_Array_Elements.addElementsToNewArray(array);
-        // Assert result
-        assertArrayEquals(new int[]{33, 42, 51}, result);
-    }
-
-    @Test
-    public void return2ElementsInArrayList() {
-        int[] array = {3, 4}; // Initial array
-        int[] result = C05_P01_Display_Array_Elements.addElementsToNewArray(array);
-        // Assert result
-        assertArrayEquals(new int[]{3, 4}, result);
-    }
-
-
 }
