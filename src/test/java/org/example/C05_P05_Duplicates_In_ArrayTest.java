f08300c5cd165ab4a9de7a81bdda34d89058a733
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-30 22:35:23 +0200

    update test dublicates in array

--- src/test/java/org/example/C05_P05_Duplicates_In_ArrayTest.java
+++ src/test/java/org/example/C05_P05_Duplicates_In_ArrayTest.java
@@ -30,4 +30,13 @@ class C05_P05_Duplicates_In_ArrayTest {
         // Assert result
         assertEquals(3, result);
     }
+
+    @Test
+    public void find0DuplicatesInArray() {
+        int[] array = {1,2,3,4,5}; // Initial array
+        // Call logic
+        int result = C05_P05_Duplicates_In_Array.findDuplicateInArray(array);
+        // Assert result
+        assertEquals(0, result);
+    }
 }
