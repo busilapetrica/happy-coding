2217b393c35c21a347bb8a9953977669b65b3da3
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-01-30 22:25:41 +0200

    update test for aditional of 2 matrix

--- src/test/java/org/example/C05_P19_Additional_Of_2_MatrixTest.java
+++ src/test/java/org/example/C05_P19_Additional_Of_2_MatrixTest.java
@@ -8,20 +8,29 @@ class C05_P19_Additional_Of_2_MatrixTest {
 
     @Test
     public void returnAdditionalOf2dMatrix() {
-        int[][] array1 = {{1, 2}, {3, 4}};
-        int[][] array2 = {{5, 6}, {7, 8}};
+        int[][] matrix1 = {{1, 2}, {3, 4}};
+        int[][] matrix2 = {{5, 6}, {7, 8}};
 
-        int[][] result = C05_P19_Additional_Of_2_Matrix.calculateAdditionalMatrix(array1, array2);
+        int[][] result = C05_P19_Additional_Of_2_Matrix.calculateAdditionalMatrix(matrix1, matrix2);
         assertArrayEquals(new int[][]{{6, 8}, {10, 12}}, result);
     }
 
     @Test
-    public void returnAdditionalOf2DifferentMatrix() {
-        int[][] array1 = {{1, 2}, {3, 4}};
-        int[][] array2 = {{5, 6, 9}, {7, 8, 10}};
+    public void returnAdditionalOf2x2And3x3Matrix() {
+        int[][] matrix1 = {{1, 2}, {3, 4}};
+        int[][] matrix2 = {{5, 6, 9}, {7, 8, 10}};
 
-        int[][] result = C05_P19_Additional_Of_2_Matrix.calculateAdditionalMatrix(array1, array2);
+        int[][] result = C05_P19_Additional_Of_2_Matrix.calculateAdditionalMatrix(matrix1, matrix2);
         assertArrayEquals(new int[][]{{6, 8, 9}, {10, 12, 10}}, result);
+    }
+
+    @Test
+    public void returnAdditionalOf2x2And2X1Matrix() {
+        int[][] matrix1 = {{1, 2}, {3, 4}};
+        int[][] matrix2 = {{5, 6}};
+
+        int[][] result = C05_P19_Additional_Of_2_Matrix.calculateAdditionalMatrix(matrix1, matrix2);
+        assertArrayEquals(new int[][]{{6, 8}, {3, 4}}, result);
     }
 
 }
