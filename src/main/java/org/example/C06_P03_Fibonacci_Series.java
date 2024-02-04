b1d030e46d3a0d8b31b0b51c0ea54c740d5fbd08
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-02-04 14:16:03 +0200

    add problem and test for Fibonacci series using recursion

---
+++ src/main/java/org/example/C06_P03_Fibonacci_Series.java
@@ -0,0 +1,32 @@
+package org.example;
+/*************************************************
+ Write a program in java to print the Fibonacci Series using recursion. >
+ Test Data :
+ Input number of terms for the Series (< 20) : 10
+ Expected Output:
+
+ Input number of terms for the Series (< 20) : 10
+ The Series are :
+ 1  1  2  3  5  8  13  21  34  55
+
+ F(n)=F(n−1)+F(n−2)
+ **************************************************/
+public class C06_P03_Fibonacci_Series {
+    public C06_P03_Fibonacci_Series() {
+    }
+    static String fibonacciStringConstruct(int fibNumberLength) {
+        String result = "";
+
+        for (int i = 1; i <= fibNumberLength; i++) {
+            result += fibonacciSeries(i) + " ";
+        }
+        return result;
+    }
+
+    public static int fibonacciSeries(int count) {
+        if (count == 1 || count == 2) {
+            return 1;
+        }
+        return fibonacciSeries(count - 1) + fibonacciSeries(count - 2);
+    }
+}
