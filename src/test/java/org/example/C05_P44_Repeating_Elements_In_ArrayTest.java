abaf85f6f019db252023d72280dd40386d3f2323
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-02-04 16:13:09 +0200

    update test for repeating elements in array

--- src/test/java/org/example/C05_P44_Repeating_Elements_In_ArrayTest.java
+++ src/test/java/org/example/C05_P44_Repeating_Elements_In_ArrayTest.java
@@ -6,8 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class C05_P44_Repeating_Elements_In_ArrayTest {
     @Test
-    public void return4ElementsRepeatedInArray() {
+    public void return2ElementsRepeatedInArray() {
         int[] array = new int[]{2, 7, 4, 7, 8, 3, 4};
+        int[] result = C05_P44_Repeating_Elements_In_Array.findAndPrintRepeatingElements(array);
+        assertArrayEquals(new int[]{7, 4}, result);
+    }
+
+    @Test
+    public void return2ElementsRepeatedInArrayWhere1ElementRepeated3Times() {
+        int[] array = new int[]{2, 7, 4, 7, 8, 7, 4};
         int[] result = C05_P44_Repeating_Elements_In_Array.findAndPrintRepeatingElements(array);
         assertArrayEquals(new int[]{7, 4}, result);
     }
