5f58753c6313ec52152a2b6a17a8e0d41f8d04e7
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-03-12 21:39:59 +0200

    add problem and test for max number count in array

---
+++ src/main/java/org/example/c06challenges/MaxNumberCountInArray.java
@@ -0,0 +1,36 @@
+package org.example.c06challenges;
+
+public class MaxNumberCountInArray {
+    private MaxNumberCountInArray() {
+    }
+
+    public static int majorityElementOfArray(int[] array) {
+        int arrayLength = array.length;
+        int max_count = 0;
+        int maxFreqNumber = -1;
+
+        if (arrayLength == 0) {
+            return -2;
+        } else {
+
+            for (int i = 0; i < arrayLength; i++) {
+                int count = 0;
+                for (int j = 0; j < arrayLength; j++) {
+
+                    if (array[i] == array[j]) {
+                        count++;
+                    }
+                }
+                if (count > max_count) {
+                    max_count = count;
+                    maxFreqNumber = array[i];
+                }
+
+                if(max_count < 2){
+                    maxFreqNumber = -1;
+                }
+            }
+        }
+        return maxFreqNumber;
+    }
+}
